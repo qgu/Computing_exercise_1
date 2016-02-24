33f33696aa627d8f3f253c357d4efed57e722196
qgu <qg213@cam.ac.uk>
2016-02-24 19:34:10 +0000

    A working version. Speed is alright. Replaced array operations in to accumulative addition.

--- monte_carlo.py
+++ monte_carlo.py
@@ -14,26 +14,31 @@ def monte_carlo_step(f, upper, lower, no_of_variables=8):
 
 def monte_carlo_integration_unit(upper, lower, no_of_samples):
     step = range(0,no_of_samples)
-    f_x = [monte_carlo_step(f, upper,lower) for ii in step]
-
-    f_x_squared = [np.power(f_x,2) for fx in f_x]
-    f_bar = np.mean(f_x)
-    f2_bar = np.mean(f_x_squared)
+    f_x = 0
+    f_x2 = 0
     
-    return [f_bar, f2_bar] 
+    for ii in step:
+        f_x_unit = monte_carlo_step(f, upper,lower)
+        f_x = f_x + f_x_unit
+        f_x2 = f_x2 + np.power(f_x_unit,2)
+
+    return [f_x, f_x2] 
 
 def monte_carlo_integration(upper, lower, no_of_samples):
-    [f_bar, f2_bar] = monte_carlo_integration_unit(upper, lower, no_of_samples)
+    M = monte_carlo_integration_unit(upper, lower, no_of_samples)
+    [f_bar, f2_bar] = np.divide(M,no_of_samples)
     
     V = np.power((upper - lower),8)
-
+    
+    print f2_bar
+    
     integration_result = V * f_bar
     error = V * np.sqrt((f2_bar-f_bar*f_bar)/no_of_samples)
 
     return [integration_result, error]
 
 t1 = time()
-print monte_carlo_integration(np.pi/8, 0, 5000)
+print monte_carlo_integration(np.pi/8, 0, 10000)
 t2 = time()
 print t2 - t1
         
