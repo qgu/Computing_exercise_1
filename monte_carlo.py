151fb7908bd0921933755b14968c6c5f19997a0e
qgu <qg213@cam.ac.uk>
2016-02-24 19:58:14 +0000

    Added write to file and repeat calculation ability.

--- monte_carlo.py
+++ monte_carlo.py
@@ -30,18 +30,27 @@ def monte_carlo_integration(upper, lower, no_of_samples):
     
     V = np.power((upper - lower),8)
     
-    print f2_bar
-    
     integration_result = V * f_bar
     error = V * np.sqrt((f2_bar-f_bar*f_bar)/no_of_samples)
 
     return [integration_result, error]
 
-t1 = time()
-print monte_carlo_integration(np.pi/8, 0, 10000)
-t2 = time()
-print t2 - t1
-        
+def estimated_computation_time(no_of_samples):
+    return 0.0000331 * no_of_samples
+
+range_of_samples = [10**x for x in range(2,5)]
+no_of_repeats = 10
+x = np.arange(0,100)
+results = np.zeros(no_of_repeats)
+
+with open('test_file','w+') as file:
+    for no_of_samples in range_of_samples:
+        for ii in range(0,no_of_repeats):
+            M = monte_carlo_integration(np.pi/8, 0, no_of_samples)
+            results[ii] = M[0]
+        file.write( str(no_of_samples) + str(np.std(results))[1:-2] )
+    
+
     
     
 
